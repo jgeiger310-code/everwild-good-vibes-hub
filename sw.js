1bc12ef70f7d253a6ed4a3d4156a6d2f4cab08d4
jgeiger310-code <jgeiger310@gmail.com>
2026-07-15 21:21:35 -0400

    Refresh cache for restored BGCC button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "everwild-good-vibes-hub-v1";
+const CACHE = "everwild-good-vibes-hub-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
